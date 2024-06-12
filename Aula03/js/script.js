f3cf28c7b917d52f5c30e47e818242dad9b9d7c6
Beatriz <charlottebeatrizn@gmail.com>
2024-06-12 14:58:28 -0300

    entrega2

--- Aula03/js/script.js
+++ Aula03/js/script.js
@@ -10,8 +10,10 @@ const isLight = document.documentElement.classList.contains("light");
 
 const imagem = isLight ? "./img/fotodeperfil.jpeg" : "./img/perfil (2).png";
 
-document.querySelector("#perfil img").setAttribute("src", imagem)
+const alert = isLight ? "uma menina na avenida paulista de calça jeans clara e blusa branca em uma foto de corpo todo" : "imagem de perfil de uma menina sorrindo para um bolo de aniversario com uma decoração em prata e branco no fundo";
 
-document.addEventListener.classList("perfil")
-};
+document.querySelector("#perfil img").setAttribute("src", imagem);
+document.querySelector("#perfil img").setAttribute("alt", alert);
+
+}
 
